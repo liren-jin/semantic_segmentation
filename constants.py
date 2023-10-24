19b5ce0cf1678dfd2f99c44b272a5ac0566b380e
Liren Jin <ljin@uni-bonn.de>
2023-10-24 13:52:43 +0200

    add train and test

---
+++ constants.py
@@ -0,0 +1,14 @@
+class Models:
+    ERFNET = "erfnet"
+    ERFNET_W_ALEATORIC = "erfnet_w_aleatoric"
+    UNET = "unet"
+    UNET_W_ALEATORIC = "unet_w_aleatoric"
+
+
+class Losses:
+    CROSS_ENTROPY = "xentropy"
+    MSE = "mse"
+    NLL = "nll"
+
+
+IGNORE_INDEX = {"cityscapes": 19, "potsdam": 0, "flightmare": 9, "shapenet": None}
